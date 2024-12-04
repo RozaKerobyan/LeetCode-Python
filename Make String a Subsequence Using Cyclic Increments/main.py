7a638e9ecf6561bbc9f3d0099ad0243f456f116f
Roza <rozaqeropyan1234@gmail.com>
2024-12-04 22:27:50 +0400

    upload

---
+++ Make String a Subsequence Using Cyclic Increments/main.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def canMakeSubsequence(self, str1, str2):
+        index = 0
+        s2_length = len(str2)
+        for i in str1:
+            if index < s2_length and (ord(str2[index]) - ord(i)) % 26 < 2:
+                index += 1
+        return index == s2_length
+
+sol = Solution()
+print(sol.canMakeSubsequence("abc", "ad"))
