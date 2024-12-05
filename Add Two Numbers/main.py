96fdf8a8562d7886f26827b3b48465d25673244a
Roza Kerobyan <97788153+RozaKerobyan@users.noreply.github.com>
2024-12-06 00:30:19 +0400

    Delete Add Two Numbers directory

--- Add Two Numbers/main.py
+++
@@ -1,7 +0,0 @@
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: Optional[ListNode]
-        :type l2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
